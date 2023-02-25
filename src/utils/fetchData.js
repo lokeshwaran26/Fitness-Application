f94a54c06333a6e9f0b832e9975d010b7bd837dd
lokesh <lokesh95664@gmail.com>
2023-02-25 19:13:20 +0530

    ..

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,9 @@
 export const exerciseOptions = {
   method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
 
